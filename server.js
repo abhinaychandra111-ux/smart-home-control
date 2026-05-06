e552333bbf9f28b560e7f1f31a11861e6be295b8
abhinaychandra111-ux <abhinaychandra111@gmail.com>
2026-05-06 18:58:49 +0530

    mongodb atlas setup

--- server.js
+++ server.js
@@ -310,7 +310,8 @@ socket.on("addRoom", async (roomName) => {
 });
 
 
-// ===== START SERVER =====
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
